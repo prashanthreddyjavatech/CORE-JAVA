6511987abb0465e8068f501d001ce4f3d2969a6a
prashanthreddyjavatech <61052176+prashanthreddyjavatech@users.noreply.github.com>
2022-03-07 10:54:48 +0530

    test sync

--- src/methods/MethodExample.java
+++ src/methods/MethodExample.java
@@ -4,10 +4,10 @@ public class MethodExample {
 	
 	public static void main(String[] args) {
 		myFirstMethod();
-	}
+	  }
 	
-	public static void myFirstMethod() {
-		System.out.println("myFirstMethod call executed.");
+	 public static void myFirstMethod() {
+		System.out.println("myFirstMethod call executed");
 	}
 
 }
